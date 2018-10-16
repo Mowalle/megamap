cd30f45d2fa0a57e5c18e6efee257a08ded67863
Ralf Manuel Morawe <rmorawe@fb3.uni-bremen.de>
2018-10-16 15:30:59 +0200

    Add task switcher, start position point and wall target
    The idea is that before executing the search and rotation targets,
    the used stands at a predefined position and has to look at a predefined target
    on the wall. By doing this, the acutal trials are started, mostly removing a
    rotation bias... (?)

---
+++ Megamap/Assets/Scripts/TaskSwitcher.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Megamap {
+
+    public class TaskSwitcher : MonoBehaviour {
+
+        public enum Type {
+            Searching, Pointing, PositionReset, GazeReset
+        }
+
+        [SerializeField]
+        private GameObject taskDisplay;
+
+        private Type currentType = Type.PositionReset;
+
+        public Type GetCurrentType() { return currentType; }
+
+        public void SwitchTask(Type taskType)
+        {
+            currentType = taskType;
+        }
+        
+        private void Start()
+        {
+            if (taskDisplay == null) {
+                Debug.LogError("TastSwitcher: Reference to taskDisplay not set; disabling script.");
+                enabled = false;
+                return;
+            }
+        }
+
+        private void Update()
+        {
+            string task = "";
+            switch (currentType) {
+            case Type.PositionReset:
+                task = "Bitte stelle dich auf das 'X'.";
+                break;
+            case Type.GazeReset:
+                task = "Bitte schaue auf das Ziel an der Wand.";
+                break;
+            case Type.Searching:
+                task = "Suche den nächsten Raum.";
+                break;
+            case Type.Pointing:
+                task = "Zeige dorthin, wo sich der ausgewählte Raum befindet.";
+                break;
+            default: break;
+            }
+
+            taskDisplay.GetComponent<Text>().text = task;
+        }
+    }
+
+}
+
+
---
+++ Megamap/Assets/Scripts/WallTarget.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Megamap {
+
+    public class WallTarget : MonoBehaviour {
+        
+        private TaskSwitcher taskSwitcher;
+        
+        void Start()
+        {
+            var go = GameObject.Find("TaskSwitcher");
+            if (go != null) {
+                taskSwitcher = go.GetComponent<TaskSwitcher>();
+            }
+            else {
+                Debug.LogError("WallTarget: TaskSwitcher cannot be found; disabling script.");
+                enabled = false;
+                return;
+            }
+        }
+
+        void Update()
+        {
+            // Only execute when player should look at wall target.
+            if (taskSwitcher.GetCurrentType() != TaskSwitcher.Type.GazeReset) {
+                return;
+            }
+
+            var cam = Camera.main;
+            RaycastHit hit;
+            // Is the player looking at this wall target? -> Start with search task.
+            if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity)) {
+                if (hit.collider.gameObject.Equals(this.gameObject)) {
+                    taskSwitcher.SwitchTask(TaskSwitcher.Type.Searching);
+                }
+            }
+        }
+    }
+
+}
