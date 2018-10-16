4c70e66ef3e80d32c8c8db429a057dfe817541cc
Ralf Manuel Morawe <rmorawe@fb3.uni-bremen.de>
2018-10-16 08:41:38 +0200

    [WIP] Create new LocationPin prefab

---
+++ Megamap/Assets/Scripts/LocationPin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationPin : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject locationPinText;
+
+    public bool isTargetPin = false;
+
+    public void showLocationPinText(bool show)
+    {
+        locationPinText.SetActive(show);
+    }
+
+    private void Start()
+    {
+        if (locationPinText == null) {
+            Debug.LogError("LocationPin: locationPinText reference not set; disabling script.");
+            enabled = false;
+            return;
+        }
+
+        showLocationPinText(false);
+    }
+}
