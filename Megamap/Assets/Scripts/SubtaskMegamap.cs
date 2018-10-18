fd6aa08774bfbfd221841ee3abea3ca53f35376f
Ralf Manuel Morawe <rmorawe@fb3.uni-bremen.de>
2018-10-18 16:28:45 +0200

    Implement pseude-randomizing of LocationPins' attribute
    The pins' attribute is now "randomized" in the following way:
    * If the pin is a target pin (toggled via editor), a random value
    between `absoluteMinimum` and `maxTargetAttributeValue` will be set for
    all target pins.
    * If the pin is not a target pin, a random value higher than
    `maxTargetAttributeValue` and lower than `maxAttributeValue` will be set
    individually for each pin.
    
    Hopefully, testers won't notice the degree of randomization, which would
    give away the target pins immediately.
    
    Closes #1.

--- Megamap/Assets/Scripts/SubtaskMegamap.cs
+++ Megamap/Assets/Scripts/SubtaskMegamap.cs
@@ -6,9 +6,34 @@ namespace Megamap {
 
     public class SubtaskMegamap : MonoBehaviour {
 
+        [SerializeField]
+        private int absoluteMinimum = 50;
+        [SerializeField]
+        private int maxTargetAttributeValue = 333;
+        [SerializeField]
+        private int maxAttributeValue = 1000;
+
         private void OnEnable()
         {
             Debug.Log("Starting the subtask \"Megamap\"");
+
+            if (maxTargetAttributeValue <= 0) {
+                Debug.LogWarning("SubtaskMegamap: Invalid maxTargetAttributeValue <= 0. Using 1 instead.");
+                maxTargetAttributeValue = 1;
+            }
+
+            if (maxAttributeValue <= maxTargetAttributeValue) {
+                maxAttributeValue = maxTargetAttributeValue + 1;
+                Debug.LogWarning("SubtaskMegamap: Invalid maxAttributeValue <= " + maxTargetAttributeValue
+                                 + ". Using " + maxAttributeValue + " instead (not a good value).");
+            }
+
+            // Determine target pins' attribute value.
+            var targetValue = Random.Range(absoluteMinimum, maxTargetAttributeValue + 1);
+            var pins = FindObjectsOfType<LocationPin>();
+            foreach (LocationPin pin in pins) {
+                pin.attribute = pin.IsTargetPin ? targetValue : Random.Range(maxTargetAttributeValue + 1, maxAttributeValue);
+            }
         }
 
     }
