351c318bfc13a615f669af3aa8198e80ff19caf2
Ralf Manuel Morawe <rmorawe@fb3.uni-bremen.de>
2018-10-19 15:48:58 +0200

    Implement very bad first implementation for laser pointer
    A simple laser object is updated so that its transform follows a
    predefined hand.

---
+++ Megamap/Assets/Scripts/SubtaskPointing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+namespace Megamap {
+
+    public class SubtaskPointing : MonoBehaviour {
+
+        public SteamVR_Action_Boolean action;
+        public Hand hand;
+        public GameObject laser;
+
+        private string taskDescription = "Zeige dorthin, wo sich der ausgewählte Raum befindet.";
+        private string confirmation = "Zum Bestätigen, Trigger betätigen.";
+
+        private void OnEnable()
+        {
+            FindObjectOfType<TaskSwitcher>().SetTaskDescription(taskDescription);
+        }
+
+        private void Update()
+        {
+            laser.transform.position = hand.objectAttachmentPoint.position;
+            laser.transform.rotation = hand.transform.rotation;
+        }
+    }
+
+}
