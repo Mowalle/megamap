6c19428bfeea94d4790ac71fcd0de1bdcb4bee63
Ralf Manuel Morawe <rmorawe@fb3.uni-bremen.de>
2018-10-20 11:09:54 +0200

    Confirm laser with trigger or go back with trackpad
    The laser freezes in the room when pulling the trigger (to confirm the
    direction). By pressing on the trackpad, the laser is unfreezed and can
    be repositioned.
    
    Closes #11.

--- Megamap/Assets/Scripts/SubtaskPointing.cs
+++ Megamap/Assets/Scripts/SubtaskPointing.cs
@@ -8,25 +8,56 @@ namespace Megamap {
 
     public class SubtaskPointing : MonoBehaviour {
 
-        public SteamVR_Action_Boolean action;
+        public SteamVR_Action_Boolean acceptAction;
+        public SteamVR_Action_Boolean backAction;
         public LineRenderer laser;
 
         private string taskDescription = "Zeige dorthin, wo sich der ausgewählte Raum befindet.";
-        private string confirmation = "Zum Bestätigen, Trigger betätigen.";
+        private string confirmation = "OK: Trigger\nKorrigieren: Trackpad";
 
         [SerializeField]
         private SteamVR_Input_Sources preferredHandType = SteamVR_Input_Sources.RightHand;
+        private Hand hand;
+
+        private bool laserLocked = false;
+
                
         private void OnEnable()
         {
             FindObjectOfType<TaskSwitcher>().SetTaskDescription(taskDescription);
+
+            hand = GetTargetHand();
+            laserLocked = false;
+        }
+
+        private void Update()
+        {
+            if (acceptAction.GetStateDown(hand.handType)) {
+                if (!laserLocked) {
+                    laserLocked = true;
+                    FindObjectOfType<TaskSwitcher>().SetTaskDescription(confirmation);
+                } 
+                else {
+                    // TODO: Record data etc...
+                    // ...
+
+                    // Do next trial.
+                    FindObjectOfType<TaskSwitcher>().SwitchTask(TaskSwitcher.Type.UserPositionSetup);
+                }
+            }
+            else if (backAction.GetStateDown(hand.handType) && laserLocked) {
+                laserLocked = false;
+                FindObjectOfType<TaskSwitcher>().SetTaskDescription(taskDescription);
+            }
         }
 
         // Need to use LateUpdate() to follow hand because otherwise hand's position will be constant
         // (probably because it is moved in a script each Update()).
         private void LateUpdate()
         {
-            var hand = GetTargetHand();
+            if (laserLocked)
+                return;
+            
             laser.SetPosition(0, hand.transform.position);
             laser.SetPosition(1, hand.transform.TransformDirection(Vector3.forward * 2000));
         }
