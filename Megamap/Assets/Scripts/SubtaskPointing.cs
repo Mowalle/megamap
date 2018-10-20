0ef9fd34c3d1485c94f70c0c7c958f2943c9ba11
Ralf Manuel Morawe <rmorawe@fb3.uni-bremen.de>
2018-10-20 10:28:18 +0200

    Make laser pointer follow controller's position and orientation correctly
    Instead of dealing with the transform, the position properties of the
    line renderer are now modified. The laser pointer is automatically
    attached to the preferred hand if available.
    
    Closes #10.

--- Megamap/Assets/Scripts/SubtaskPointing.cs
+++ Megamap/Assets/Scripts/SubtaskPointing.cs
@@ -9,21 +9,46 @@ namespace Megamap {
     public class SubtaskPointing : MonoBehaviour {
 
         public SteamVR_Action_Boolean action;
-        public Hand hand;
-        public GameObject laser;
+        public LineRenderer laser;
 
         private string taskDescription = "Zeige dorthin, wo sich der ausgewählte Raum befindet.";
         private string confirmation = "Zum Bestätigen, Trigger betätigen.";
 
+        [SerializeField]
+        private SteamVR_Input_Sources preferredHandType = SteamVR_Input_Sources.RightHand;
+               
         private void OnEnable()
         {
             FindObjectOfType<TaskSwitcher>().SetTaskDescription(taskDescription);
         }
 
-        private void Update()
+        // Need to use LateUpdate() to follow hand because otherwise hand's position will be constant
+        // (probably because it is moved in a script each Update()).
+        private void LateUpdate()
         {
-            laser.transform.position = hand.objectAttachmentPoint.position;
-            laser.transform.rotation = hand.transform.rotation;
+            var hand = GetTargetHand();
+            laser.SetPosition(0, hand.transform.position);
+            laser.SetPosition(1, hand.transform.TransformDirection(Vector3.forward * 2000));
+        }
+
+        // TODO: Same code as in HandDisplay -> Refactor?
+        private Hand GetTargetHand()
+        {
+            var hands = FindObjectsOfType<Hand>();
+            if (hands.Length == 0) {
+                return null;
+            } else if (hands.Length == 1) {
+                return hands[0];
+            } else {
+                // If preferred hand type was found (left or right handed), use it.
+                // Otherwise, use the first hand found.
+                foreach (Hand hand in hands) {
+                    if (hand.handType == preferredHandType) {
+                        return hand;
+                    }
+                }
+                return hands[0];
+            }
         }
     }
 
