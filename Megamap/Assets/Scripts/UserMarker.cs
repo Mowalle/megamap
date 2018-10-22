98d3db75b3692344465e2013616e452dba965eb0
Ralf Manuel Morawe <rmorawe@fb3.uni-bremen.de>
2018-10-22 11:31:49 +0200

    Implement 3D user marker for showing position
    The marker is placed according to two reference points, one attached to
    the lab GO and one attached to the Megamap GO. The attachment is done
    manually in the editor for now.
    
    Closes #17.

---
+++ Megamap/Assets/Scripts/UserMarker.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Megamap {
+
+    public class UserMarker : MonoBehaviour {
+        
+        [SerializeField]
+        private Transform labReferenceTransform;
+        [SerializeField]
+        private Transform mapReferenceTransform;
+        [SerializeField]
+        private Camera cam;
+
+        private void Start()
+        {
+            if (cam == null) {
+                cam = Camera.main;
+            }
+        }
+
+        private void Update()
+        {
+            var megamapConfig = FindObjectOfType<MegamapConfiguration>();
+            if (megamapConfig == null) {
+                Debug.LogError("UserMarker: No Megamap object found.");
+                return;
+            }
+
+            var offset = cam.transform.position - labReferenceTransform.position;
+            var newPosition = mapReferenceTransform.position + offset * megamapConfig.scale;
+            transform.localPosition = new Vector3(newPosition.x, megamapConfig.heightOffset, newPosition.z);
+
+            transform.rotation = Quaternion.Euler(0f, cam.transform.rotation.eulerAngles.y, 0f);
+            transform.localScale = new Vector3(megamapConfig.scale, megamapConfig.scale, megamapConfig.scale);
+        }
+    }
+
+}
