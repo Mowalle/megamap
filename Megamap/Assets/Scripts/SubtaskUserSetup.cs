c88eceb00b5decd2dfb89dc534c9115246bd5368
Ralf Manuel Morawe <rmorawe@fb3.uni-bremen.de>
2018-10-30 10:20:14 +0100

    Move start positions of user setup
    By using a specifically set point for each task, it is less likely that
    the Megamap will clip the walls.
    
    Fixes #29.

--- Megamap/Assets/Scripts/SubtaskUserSetup.cs
+++ Megamap/Assets/Scripts/SubtaskUserSetup.cs
@@ -35,6 +35,17 @@ namespace Megamap {
             floorTarget.gameObject.SetActive(true);
             wallTarget.gameObject.SetActive(false);
             selectionRadial.Hide();
+
+            // Re-Center task display and wall target to be in line with floor target.
+            var taskDisplay = FindObjectOfType<TaskDisplay>();
+            taskDisplay.transform.position = new Vector3(floorTarget.transform.position.x,
+                                                         taskDisplay.transform.position.y,
+                                                         taskDisplay.transform.position.z);
+
+
+            wallTarget.transform.position = new Vector3(floorTarget.transform.position.x,
+                                                        wallTarget.transform.position.y,
+                                                        wallTarget.transform.position.z);
         }
 
         private void OnDisable()
