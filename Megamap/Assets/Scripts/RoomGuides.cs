4ba648bdb1f2fb93b8dda6c2a5f8863bda9e33f0
Ralf Manuel Morawe <rmorawe@fb3.uni-bremen.de>
2018-11-01 11:40:56 +0100

    Add graphical room guides (by default for upper corners only)
    Closes #33.

--- Megamap/Assets/Scripts/RoomGuides.cs
+++ Megamap/Assets/Scripts/RoomGuides.cs
@@ -6,14 +6,27 @@ public class RoomGuides : MonoBehaviour {
 
     [SerializeField] private Renderer labRoomRenderer;
     [SerializeField] private Renderer mapRoomRenderer;
+    [SerializeField] private bool showUpperOnly = true;
 
-    private void Update()
+    private LineRenderer[] lines;
+
+    private void Awake()
+    {
+        lines = GetComponentsInChildren<LineRenderer>();
+    }
+
+    private void LateUpdate()
     {
         var labPoints = GetBoundsPoints(labRoomRenderer.bounds);
         var roomPoints = GetBoundsPoints(mapRoomRenderer.bounds);
 
         for (int i = 0; i < 8; ++i) {
-            Debug.DrawLine(labPoints[i], roomPoints[i], Color.red);
+            lines[i].SetPosition(0, labPoints[i]);
+            lines[i].SetPosition(1, roomPoints[i]);
+        }
+
+        for (int i = 0; i < 4; ++i) {
+            lines[i].enabled = !showUpperOnly;
         }
     }
 
@@ -21,10 +34,13 @@ public class RoomGuides : MonoBehaviour {
     {
         var points = new Vector3[8];
 
+        // Lower four points.
         points[0] = bounds.min;
         points[1] = bounds.min + new Vector3(bounds.size.x, 0f, 0f);
         points[2] = bounds.min + new Vector3(0f, 0f, bounds.size.z);
         points[3] = bounds.min + new Vector3(bounds.size.x, 0f, bounds.size.z);
+
+        // Upper four points.
         points[4] = bounds.min + new Vector3(0f, bounds.size.y, 0f);
         points[5] = bounds.min + new Vector3(bounds.size.x, bounds.size.y, 0f);
         points[6] = bounds.min + new Vector3(0f, bounds.size.y, bounds.size.z);
