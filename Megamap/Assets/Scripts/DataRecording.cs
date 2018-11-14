ceb153a4abf87ff0c052916b4a9f265ea8643176
Ralf Manuel Morawe <mowalle.dev@gmail.com>
2018-11-14 23:16:44 +0100

    Create new user folder for eval results on each run
    Each user folder has the format of
    "user_ID_YYYY-MM-DD_HH-mm-ss", where the ID is an integer that is
    automatically detected at the start of the program based on all existing
    user folders within the "Results" folder. The ID is simply incremented
    by one on each run.

--- Megamap/Assets/Scripts/DataRecording.cs
+++ Megamap/Assets/Scripts/DataRecording.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class DataRecording : MonoBehaviour {
 
-    [SerializeField] private string userID = "";
+    public DirectoryInfo UserFolder { get; set; }
 
-    DirectoryInfo baseDir = null;
+    [SerializeField] private string userID = "";
+    private string startTime = "";
 
     private void Start()
     {
-        baseDir = Directory.GetParent(Application.dataPath);
+        Assert.raiseExceptions = true;
+
+        startTime = DateTime.UtcNow.ToString("yyyy-MM-dd_HH_mm_ss");
+
+        DirectoryInfo baseDir = Directory.GetParent(Application.dataPath);
 
         try {
             DirectoryInfo di = new DirectoryInfo(baseDir.FullName + "/Results");
@@ -20,10 +27,41 @@ public class DataRecording : MonoBehaviour {
             else {
                 di.Create();
             }
+
+            var userDirs = di.GetDirectories("user_*");
+            userID = "user_" + NextUserID(userDirs);
+
+            UserFolder = new DirectoryInfo(di.FullName + "/" + userID + "_" + startTime);
+            // Just to make sure...
+            Debug.Assert(!UserFolder.Exists, "User folder would be overwritten; ABORT!");
+            UserFolder.Create();
         }
         catch (Exception e) {
             Debug.LogError("Creating directory failed: " + e.ToString());
         }
+    }
+
+    private string NextUserID(DirectoryInfo[] dirs)
+    {
+        List<string> dirNames = new List<string>();
+
+        foreach (var dir in dirs) {
+            if (dir.Name.StartsWith("user_")) {
+                dirNames.Add(dir.Name.Substring("user_".Length));
+            }
+        }
+
+        dirNames.Sort();
+
+        for (int i = dirNames.Count - 1; i >= 0; --i) {
+            var fields = dirNames[i].Split('_');
+            int n;
+            if (int.TryParse(fields[0], out n)) {
+                return (n + 1).ToString();
+            }
+        }
+
+        return "0";
     }
 }
 
