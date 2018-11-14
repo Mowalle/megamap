20c77d739ffddf1838e349b1dfb1ace63c9f0cbe
Ralf Manuel Morawe <mowalle.dev@gmail.com>
2018-11-14 17:36:16 +0100

    Create base folder for eval results if it does not exists

---
+++ Megamap/Assets/Scripts/DataRecording.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+public class DataRecording : MonoBehaviour {
+
+    [SerializeField] private string userID = "";
+
+    DirectoryInfo baseDir = null;
+
+    private void Start()
+    {
+        baseDir = Directory.GetParent(Application.dataPath);
+
+        try {
+            DirectoryInfo di = new DirectoryInfo(baseDir.FullName + "/Results");
+            if (di.Exists) {
+                Debug.Log("Not creating directory \"Results\": already exists.");
+            }
+            else {
+                di.Create();
+            }
+        }
+        catch (Exception e) {
+            Debug.LogError("Creating directory failed: " + e.ToString());
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
