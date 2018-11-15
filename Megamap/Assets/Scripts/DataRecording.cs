574e5ada18ccbebded315697555c1004ba567217
Ralf Manuel Morawe <mowalle.dev@gmail.com>
2018-11-15 14:05:59 +0100

    Put DataRecording into Megamap namespace

--- Megamap/Assets/Scripts/DataRecording.cs
+++ Megamap/Assets/Scripts/DataRecording.cs
@@ -4,97 +4,101 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.Assertions;
 
-public class DataRecording : MonoBehaviour {
+namespace Megamap {
 
-    public DirectoryInfo UserFolder { get; set; }
+    public class DataRecording : MonoBehaviour {
 
-    public bool writeData = true;
+        public DirectoryInfo UserFolder { get; set; }
 
-    [SerializeField] private string userID = "";
-    private string startTime = "";
+        public bool writeData = true;
 
-    private StreamWriter writer = null;
+        [SerializeField] private string userID = "";
+        private string startTime = "";
 
-    public static DirectoryInfo IncrementDirectory(DirectoryInfo rootDir, string dirNameStem, string suffix)
-    {
-        var dirs = rootDir.GetDirectories();
+        private StreamWriter writer = null;
 
-        List<string> dirNames = new List<string>();
+        public static DirectoryInfo IncrementDirectory(DirectoryInfo rootDir, string dirNameStem, string suffix)
+        {
+            var dirs = rootDir.GetDirectories();
 
-        foreach (var dir in dirs) {
-            if (dir.Name.StartsWith(dirNameStem)) {
-                dirNames.Add(dir.Name.Substring(dirNameStem.Length));
+            List<string> dirNames = new List<string>();
+
+            foreach (var dir in dirs) {
+                if (dir.Name.StartsWith(dirNameStem)) {
+                    dirNames.Add(dir.Name.Substring(dirNameStem.Length));
+                }
+            }
+
+            dirNames.Sort();
+
+            string newDirNumber = "0";
+            for (int i = dirNames.Count - 1; i >= 0; --i) {
+                var fields = dirNames[i].Split('_');
+                int n;
+                if (int.TryParse(fields[0], out n)) {
+                    newDirNumber = (n + 1).ToString();
+                    break;
+                }
+            }
+
+            var newDir = new DirectoryInfo(rootDir.FullName + "/" + dirNameStem + newDirNumber + suffix);
+            // Just to make sure...
+            Debug.Assert(!newDir.Exists, "User folder would be overwritten; ABORT!");
+            newDir.Create();
+            return newDir;
+        }
+
+        private void Awake()
+        {
+            enabled = writeData;
+        }
+
+        private void Start()
+        {
+            Assert.raiseExceptions = true;
+
+            CreateUserDir();
+
+            writer = File.AppendText(UserFolder.FullName + "/position_and_view.csv");
+        }
+
+        private void OnDestroy()
+        {
+            writer.Close();
+        }
+
+        private void LateUpdate()
+        {
+            var cam = Camera.main.transform;
+            writer.WriteLine(Time.time + ", "
+                + cam.position.x + ", "
+                + cam.position.y + ", "
+                + cam.position.z + ", "
+                + cam.rotation.eulerAngles.x + ", "
+                + cam.rotation.eulerAngles.y + ", "
+                + cam.rotation.eulerAngles.z);
+        }
+
+        private void CreateUserDir()
+        {
+            startTime = DateTime.UtcNow.ToString("yyyy-MM-dd_HH_mm_ss");
+
+            DirectoryInfo baseDir = Directory.GetParent(Application.dataPath);
+
+            try {
+                DirectoryInfo resultsDir = baseDir.CreateSubdirectory("Results");
+
+                UserFolder = IncrementDirectory(resultsDir, "user_", "_" + startTime);
+                resultsDir.CreateSubdirectory(UserFolder.Name);
+
+                userID = UserFolder.Name;
+            } catch (Exception e) {
+                Debug.LogError("Creating directory failed: " + e.ToString());
+                enabled = false;
             }
         }
-
-        dirNames.Sort();
-
-        string newDirNumber = "0";
-        for (int i = dirNames.Count - 1; i >= 0; --i) {
-            var fields = dirNames[i].Split('_');
-            int n;
-            if (int.TryParse(fields[0], out n)) {
-                newDirNumber = (n + 1).ToString();
-                break;
-            }
-        }
-
-        var newDir = new DirectoryInfo(rootDir.FullName + "/" + dirNameStem + newDirNumber + suffix);
-        // Just to make sure...
-        Debug.Assert(!newDir.Exists, "User folder would be overwritten; ABORT!");
-        newDir.Create();
-        return newDir;
     }
 
-    private void Awake()
-    {
-        enabled = writeData;
-    }
-
-    private void Start()
-    {
-        Assert.raiseExceptions = true;
-
-        CreateUserDir();
-
-        writer = File.AppendText(UserFolder.FullName + "/position_and_view.csv");
-    }
-
-    private void OnDestroy()
-    {
-        writer.Close();
-    }
-
-    private void LateUpdate()
-    {
-        var cam = Camera.main.transform;
-        writer.WriteLine(Time.time + ", "
-            + cam.position.x + ", "
-            + cam.position.y + ", "
-            + cam.position.z + ", "
-            + cam.rotation.eulerAngles.x + ", "
-            + cam.rotation.eulerAngles.y + ", "
-            + cam.rotation.eulerAngles.z);
-    }
-
-    private void CreateUserDir()
-    {
-        startTime = DateTime.UtcNow.ToString("yyyy-MM-dd_HH_mm_ss");
-
-        DirectoryInfo baseDir = Directory.GetParent(Application.dataPath);
-
-        try {
-            DirectoryInfo resultsDir = baseDir.CreateSubdirectory("Results");
-
-            UserFolder = IncrementDirectory(resultsDir, "user_", "_" + startTime);
-            resultsDir.CreateSubdirectory(UserFolder.Name);
-
-            userID = UserFolder.Name;
-        } catch (Exception e) {
-            Debug.LogError("Creating directory failed: " + e.ToString());
-            enabled = false;
-        }
-    }
 }
 
 
